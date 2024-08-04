def90a491cdd66bd66891505219b74f1d70db24c
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2024-08-04 21:10:06 +0000

    correçõs no backend

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors';
+
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.post('/usuarios', async (req, res) => {
 
-   await prisma.user.create({
+   const user = await prisma.user.create({
         data: {
             email: req.body.email,
             name: req.body.name,
-            address: req.body.address
+            password: req.body.password
         }
     })
 
-    res.status(201).json(req.body)
+    res.status(201).json(user)
 })
 
 app.get('/usuarios', async (req, res) => {
@@ -28,12 +30,10 @@ app.get('/usuarios', async (req, res) => {
             where: {
                 name: req.query.name,
                 email: req.body.email,
-                address: req.body.address
-                
             }
         })
 
-    } else{
+    } else {
         users = await prisma.user.findMany()
     }
 
@@ -49,10 +49,10 @@ app.put('/usuarios/:id', async (req, res) => {
         data: {
              email: req.body.email,
              name: req.body.name,
-             address: req.body.address
+             password: req.body.password
          }
      })
- 
+
      res.status(201).json(req.body)
  })
 
@@ -68,7 +68,10 @@ app.delete('/usuarios/:id', async (req, res) => {
 
 })
 
+
+app.use(express.static('public'))
 app.listen(3000)
+
 
 /* 
 
